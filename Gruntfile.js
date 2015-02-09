fce08ba16ba58482627975a58f3e9355da86247e
thele116 <thele116@morris.umn.edu>
2015-02-08 20:23:14 -0600

    Altered production config for "mock" production.

--- Gruntfile.js
+++ Gruntfile.js
@@ -542,6 +542,21 @@ module.exports = function (grunt) {
     ]);
   });
 
+  grunt.registerTask('serveProd', function (target) {
+    grunt.task.run([
+      'clean:server',
+      'env:prod',
+      'concurrent:server',
+      'injector',
+      'wiredep',
+      'autoprefixer',
+      'express:dev',
+      'wait',
+      'open',
+      'watch'
+    ]);
+  });
+
   grunt.registerTask('server', function () {
     grunt.log.warn('The `server` task has been deprecated. Use `grunt serve` to start a server.');
     grunt.task.run(['serve']);
