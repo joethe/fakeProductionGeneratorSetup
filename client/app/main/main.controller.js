effe2508abbc748fe69349f935bb8c27fd0182b9
thele116 <thele116@morris.umn.edu>
2015-02-10 12:54:41 -0600

    Added basic list of students with some info as a demo. Updated model to match our sample data.

--- client/app/main/main.controller.js
+++ client/app/main/main.controller.js
@@ -9,6 +9,47 @@ angular.module('fakeProductionGeneratorSetupApp')
       socket.syncUpdates('thing', $scope.awesomeStudents);
     });
 
+    $scope.coursesInProgress = function(courses){
+      var inProgressCourses = 0;
+      for(var i = 0; i < courses.length; i++){
+        if(courses[i].grade === "IP"){
+          inProgressCourses += 1;
+        }
+      }
+      return inProgressCourses;
+    };
+
+    $scope.coursesFailed = function(courses){
+      var coursesFailed = 0;
+      for(var i = 0; i < courses.length; i++){
+        if(courses[i].grade === "F"){
+          coursesFailed += 1;
+        }
+      }
+      return coursesFailed;
+    };
+
+    $scope.isPassing = function(letter){
+      switch(letter){
+        case 'A': return true;
+        case 'B': return true;
+        case 'C': return true;
+        case 'D': return true;
+        default: return false;
+      }
+    };
+
+    $scope.coursesPassed = function(courses){
+      var coursesPassed = 0;
+      for(var i = 0; i < courses.length; i++){
+        if($scope.isPassing(courses[i].grade)){
+          coursesPassed += 1;
+        }
+      }
+      return coursesPassed;
+    };
+
+
     //$scope.addThing = function() {
     //  if($scope.newThing === '') {
     //    return;
--- server/api/student/student.model.js
+++ server/api/student/student.model.js
@@ -4,8 +4,16 @@ var mongoose = require('mongoose'),
     Schema = mongoose.Schema;
 
 var StudentSchema = new Schema({
-  name: String,
-  major: String
+  firstName: String,
+  lastName: String,
+  dateOfBirth: String,
+  gender: String,
+  email: String,
+  phone: String,
+  address: String,
+  courses: [],
+  major1: String,
+  major2: String
 });
 
 module.exports = mongoose.model('Student', StudentSchema);
